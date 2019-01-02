d19c8b3e7b931dbbf06ebb5bf3598078b94f2279
noidsoup <nicholas.eymann@gmail.com>
2019-01-01 21:05:06 -0800

    test

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@
 export default {
   name: 'App',
   mounted() {
+    // A simple Lorenz Attractor renderer
+    // Change the parameters for different results!
+    // by @liabru - http://brm.io
 
     var canvas = document.getElementById('canvas'),
         context = canvas.getContext('2d'),
